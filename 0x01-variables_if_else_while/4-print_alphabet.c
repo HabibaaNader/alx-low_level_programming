49ac4838f365ea017f9be845ac3bdd0ef95abc74
HabibaaNader <habiba.n.shabana@gmail.com>
2023-09-09 15:57:52 -0700

    a

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - alphabet
+ *
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == 'e' || letter == 'q')
+			continue;
+		else
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
