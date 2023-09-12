0d79a8662406f7b946bc29883c45e81015b199f2
HabibaaNader <habiba.n.shabana@gmail.com>
2023-09-12 14:24:58 -0700

    a

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - boutta print the times table so hard rn fr
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int table, num, prod;
+
+	for (table = 0; table < 10; table++)
+	{
+		for (num = 0; num < 10; num++)
+		{
+			prod = table * num;
+
+			/* Formatting the output */
+			if (num != 0)
+			{
+				_putchar(44);
+				_putchar(32);
+			}
+
+			if (prod >= 10)
+			{
+				_putchar((prod / 10) + 48);
+				_putchar((prod % 10) + 48);
+			}
+			else if ((prod < 10) && (num != 0))
+			{
+				_putchar(32);
+				_putchar((prod % 10) + 48);
+			}
+			else
+				_putchar((prod % 10) + 48);
+		}
+		_putchar(10);
+	}
+}
